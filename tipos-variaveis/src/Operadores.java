8eb872d09db4a720579b64a5926c60dacddca105
Ciscogean <ciscogean@gmail.com>
2024-05-07 04:51:03 -0300

    Operadores Relacionais

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,15 +1,22 @@
 public class Operadores {
     
     public static void main(String[] args) {
-        int a, b;
-
-        a = 5;
-        b = 6;
-
-        String resultado = a == b ? "verdadeiro" : "falso";
+        String nomeUm = "Cisco";
+        String nomeDois = "Cisco";
+        
+        System.out.println(nomeUm.equals(nomeDois));
         
         
-        System.out.println(resultado);
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        System.out.println("numeroUm é igual a numroDois? " + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("numeroUm é diferente de numero Dois?" + simNao);
         
     }
 }
