a557324808c7b01731fcec8c58f0efa406f69795
Ciscogean <ciscogean@gmail.com>
2024-05-07 04:35:13 -0300

    Incrementação

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -3,11 +3,13 @@ public class Operadores {
     public static void main(String[] args) {
         int numero = 5;
 
-        numero = - numero;
+        //x repetição
+        numero++;
+        //numero = numero +1;
 
-        System.out.println(numero);
-
-        numero = numero * -1;
+        System.out.println(numero++); // Imprimir o numero sem incremetação e incrementa depois
+        
+        System.out.println(++ numero); // incementa e imprimir
 
         System.out.println(numero);
         
