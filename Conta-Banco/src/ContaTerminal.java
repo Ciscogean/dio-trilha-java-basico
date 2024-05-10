130aca1075da2bf8588fdd6fcb0667a7309a58cf
Ciscogean <ciscogean@gmail.com>
2024-05-10 04:58:54 -0300

    Desafio de Projeto DIO Conta Terminal

--- Conta-Banco/src/ContaTerminal.java
+++ Conta-Banco/src/ContaTerminal.java
@@ -1,11 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        //TODO:Conhecer e importar a classe Scanner
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Digite o número da conta: ");
+        int conta = scanner.nextInt();
 
-        //Exibir as mensagens para o nosso usuário
+        System.out.println("Digite a Agencia: ");
+        String agencia = scanner.next();
 
-        //Obter pela scanner os valores digitados no terminal
+        System.out.println("Digite o nome do cliente: ");
+        String cliente = scanner.next();
 
-        //Exibir a mensagem conta criada
+        System.out.println("Digite o saldo: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + cliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + " e seu saldo " + saldo + " já está disponível para saque");
+        
     }
 }
