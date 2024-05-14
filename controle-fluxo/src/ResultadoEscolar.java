7a18ebe4b2b623cc8bf9306ed08e3a19bcdc73d5
Ciscogean <ciscogean@gmail.com>
2024-05-14 05:03:22 -0300

    Cenário 2

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,9 +1,9 @@
-// Cenário 1
+// Cenário 2
 public class ResultadoEscolar {
 	public static void main(String[] args) {
         int nota = 7;
 		
-        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
 		
         System.out.println(resultado);
 	}
